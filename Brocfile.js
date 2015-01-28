955bc0d67fe860475f1a27652d43ebdb7ad222dc
joenbob <elliottro@gmail.com>
2015-01-27 19:58:41 -0800

    Small updates

--- Brocfile.js
+++ Brocfile.js
@@ -1,20 +1,82 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var env = process.env.EMBER_ENV;
+var config = require("./config/environment")(env);
 
-var app = new EmberApp();
+var app = new EmberApp({
+  wrapInEval: false,
+});
+
+
+/**
+ * inlineContent
+ * 
+ * index.html inline content was enabled as of ember-cli 0.1.4
+ * this is baised on ember-cli-inline-content addon
+ * url: https://github.com/gdub22/ember-cli-inline-content
+ * 
+ */
+app.options.inlineContent = {
+  // "google-map": {
+  //   content: '<script type="text/javascript" src="//maps.googleapis.com/maps/api/js?v=3&libraries=places&sensor=false"></script>'
+  // },
+  // "google-analytics": {
+  //   file: "./vendor/google-analytics.js",
+  //   postProcess: function(content) {
+  //     return content.replace(/\{\{GOOGLE_ANALYTICS_ID\}\}/g, config.googleAnalyticsId);
+  //   },
+  //   enabled: process.env.EMBER_ENV === 'production'
+  // },
+  // "olark": {
+  //   file: "./vendor/olark.js",
+  //   attrs: { 'data-cfasync' : true },
+  //   enabled: process.env.EMBER_ENV === 'production'
+  // },
+  // "trackjs-options": {
+  //   file: "./vendor/trackjs.js",
+  //   enabled: process.env.EMBER_ENV === 'production'
+  // },
+  // "trackjs": {
+  //   content: '<script type="text/javascript" src="//d2zah9y47r7bi2.cloudfront.net/releases/current/tracker.js" data-token="cee3b74e64784d8bbf6b6c5e5ad04dac"></script>',
+  //   enabled: process.env.EMBER_ENV === 'production'
+  // }
+}
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
 // If you need to use different assets in different
 // environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
+// object"s keys should be the environment name and the values
 // should be the asset to use in that environment.
 //
 // If the library that you are including contains AMD or ES6
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+
+app.import({
+  development: "bower_components/ember/ember.js",
+  production:  "bower_components/ember/ember.prod.js"
+});
+
+// import jquery and jquery plugins
+app.import({
+  development: "bower_components/jquery/dist/jquery.js",
+  production: "bower_components/jquery/dist/jquery.min.js"
+});
+
+// import sly scroller plugin
+app.import({
+  development: "bower_components/sly/dist/sly.js",
+  production: "bower_components/sly/dist/sly.min.js"
+});
+
+app.import({
+  development: "bower_components/foundation/js/foundation.js",
+  production: "bower_components/foundation/js/foundation.min.js"
+})
+
 
 module.exports = app.toTree();
--- config/environment.js
+++ config/environment.js
@@ -19,12 +19,24 @@ module.exports = function(environment) {
     }
   };
 
-  if (environment === 'development') {
+ if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    
+    // Open all xss
+    ENV.contentSecurityPolicy = {
+      'default-src': "*",
+      'script-src': "* 'unsafe-inline' 'unsafe-eval'",
+      'font-src': "* ",
+      'connect-src': "* ",
+      'img-src': "* ",
+      'style-src': "* 'unsafe-inline'",
+      'media-src': "*",
+      'report-uri': "api/test"
+    }
   }
 
   if (environment === 'test') {
@@ -40,7 +52,31 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    /**
+     * Adds content security policy options to prevent xss attacks
+     *  See http://content-security-policy.com/ for more info about these options
+     */
+    ENV.contentSecurityPolicy = {
+      'default-src': "'none'",
+      // Allow scripts from google and olark
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' *.gstatic.com "+
+                    "*.googleapis.com www.google-analytics.com",
+      // Allow iframes from olark
+      'frame-src': "'self' ",
+      // Allow fonts to be loaded from google and bootstrap
+      'font-src': "'self' fonts.gstatic.com ",
+      // Allow data (ajax/websocket) from olark
+      'connect-src': "'self' *.olark.com",
+      // Allow images from google and olark
+      'img-src': "'self' *.googleapis.com *.gstatic.com"+
+                 " www.google-analytics.com",
+      // Allow css from bootstrap, google and olark
+      'style-src': "'self' 'unsafe-inline' fonts.googleapis.com ",
+      // Allow media from static.olark
+      'media-src': "'self' ",
+      // Report all infractions to api/test
+      'report-uri': "api/test"
+    }
   }
 
   return ENV;
