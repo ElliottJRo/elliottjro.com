c735187a957a2785f7fd52ec013480f1d6e28cca
joenbob <elliottro@gmail.com>
2015-01-28 11:57:44 -0800

    Update dev environment and others

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
+import ApplicationAdapter from './adapters/application';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
@@ -9,6 +10,17 @@ var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
+});
+
+App.RESTSerializer = DS.RESTSerializer.extend({
+  init: function() {
+    this._super();
+  }
+});
+
+App.Store = DS.Store.create({
+  revision: 1,
+  adapter: ApplicationAdapter
 });
 
 loadInitializers(App, config.modulePrefix);
