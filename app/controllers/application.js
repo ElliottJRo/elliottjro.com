2b72ab9fa820190e9160f2139d1b31d31f143208
joenbob <elliottro@gmail.com>
2015-02-10 19:19:22 -0800

    Saving changes

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // navigationLinks: [
-  //   {name: 'Home', route: 'index'},
-  //   {name: 'Projects', route: 'projects'},
-  //   {name: 'Blog', route: 'blog'},
-  //   {name: 'Contact', route: 'contact'}
-  // ],
+  navigationLinks: [
+    {name: 'Home', route: 'index'},
+    {name: 'Projects', route: 'projects'},
+    {name: 'Blog', route: 'blog'},
+    {name: 'Contact', route: 'contact'}
+  ],
 
   slyScrollObject: null,
 
--- app/views/application.js
+++ app/views/application.js
@@ -3,7 +3,16 @@ import Ember from "ember";
 export default Ember.View.extend({
 
   initFoundation: function() {
-    this.$(document).foundation();
+    this.$(document).foundation({
+        offcanvas : {
+        // Sets method in which offcanvas opens.
+        // [ move | overlap_single | overlap ]
+        open_method: 'overlap_single', 
+        // Should the menu close when a menu link is clicked?
+        // [ true | false ]
+        close_on_click : false
+      }
+    });
   }.on("didInsertElement"),
 
 });
