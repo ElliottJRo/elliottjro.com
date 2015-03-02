4c43fe9b34a0d87792c89e1a5c4ac136320549c9
joenbob <ejaiaro@gmail.com>
2015-03-01 17:48:18 -0800

    Remove projects route and links

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
     
     navigationLinks: [
         {name: 'Home', route: 'index'},
-        {name: 'Projects', route: 'projects'},
+        //{name: 'Projects', route: 'projects'},
         {name: 'Blog', route: 'blog'},
         {name: 'Contact', route: 'contact'}
     ],
--- app/router.js
+++ app/router.js
@@ -7,7 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
 
-    this.resource("projects");
+    //this.resource("projects");
 
     this.resource("contact");
 
