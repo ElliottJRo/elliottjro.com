af457f2473f568433ccc5e9dfeb9bdf148a25447
joenbob <ejaiaro@gmail.com>
2015-03-07 21:04:26 -0800

    re-enable projects route

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
 
-    //this.resource("projects");
+    this.resource("projects", function() {
+        
+    });
 
     this.resource("contact");
 
