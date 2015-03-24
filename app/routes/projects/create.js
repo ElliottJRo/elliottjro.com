1ba6b6e66cae9ae4ca30bdae5f09a766c281cbda
joenbob <elliottro@gmail.com>
2015-03-23 21:20:02 -0700

    Update create new project page

--- app/routes/projects/create.js
+++ app/routes/projects/create.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
+  newModel: null,
+
+  model: function() {
+    var currentModel = this.get("newModel");
+
+    if(!Ember.isEmpty(currentModel)) {
+      return currentModel;
+    } else {
+      currentModel = this.store.createRecord("project");
+      this.set("newModel", currentModel);
+      return currentModel;
+    }
+  }
+
 
 });
