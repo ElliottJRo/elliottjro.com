80c31d85a37ae13e779d1494be46766c5eff10ad
joenbob <ejaiaro@gmail.com>
2015-04-03 20:01:25 -0700

    Update project creation route and templates

--- app/components/image-uploader.js
+++
@@ -1,8 +0,0 @@
-import Ember from "ember";
-import DropletMixin from "elliottjro/mixins/droplet-mixin";
-
-export default Ember.Component.extend(DropletMixin, {
-
-    dropletUrl: "localhost:4200",
-
-});
